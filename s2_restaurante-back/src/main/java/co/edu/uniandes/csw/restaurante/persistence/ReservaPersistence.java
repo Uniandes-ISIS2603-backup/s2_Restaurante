d97c129f09da8ad0238c84857c01e6fc82b94dab
Juan Ortega <ja.ortega@uniandes.edu.co>
2018-09-13 14:23:51 -0500

    Modificadas las clases ReservaPersistence y ReservaLogicTest

--- s2_restaurante-back/src/main/java/co/edu/uniandes/csw/restaurante/persistence/ReservaPersistence.java
+++ s2_restaurante-back/src/main/java/co/edu/uniandes/csw/restaurante/persistence/ReservaPersistence.java
@@ -38,7 +38,7 @@ public class ReservaPersistence {
         LOGGER.log(Level.INFO, "Creando una reserva nueva");
         /* Note que hacemos uso de un método propio de EntityManager para persistir la reserva en la base de datos.
         Es similar a "INSERT INTO table_name (column1, column2, column3, ...) VALUES (value1, value2, value3, ...);" en SQL.
-         */
+         */ 
         em.persist(reservaEntity);
         LOGGER.log(Level.INFO, "Saliendo de crear una reserva nueva");
         return reservaEntity;
