da7a4f71ad0f058a82ddbd12696ccaea30818434
jp.romero12 <jp.romero12@uniandes.edu.co>
2018-09-19 21:12:46 -0500

    Mas clases

---
+++ s2_restaurante-back/src/main/java/co/edu/uniandes/csw/restaurante/ejb/SucursalMesaLogic.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.restaurante.ejb;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author jp.romero12
+ */
+@Stateless
+public class SucursalMesaLogic {
+    
+    
+    
+}
