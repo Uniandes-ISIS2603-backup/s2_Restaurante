b05817133f2697d5e61a08ccc97788f765000d00
juan <jp.hidalgo@uniandes.edu.co>
2018-11-08 08:43:43 -0500

    arreglo issue 32

--- s2_restaurante-api/src/main/java/co/edu/uniandes/csw/restaurante/resources/SucursalReservasResource.java
+++ s2_restaurante-api/src/main/java/co/edu/uniandes/csw/restaurante/resources/SucursalReservasResource.java
@@ -13,12 +13,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 
 /**
@@ -26,6 +29,10 @@ import javax.ws.rs.WebApplicationException;
  * la entidad de Sucursal y Reserva.
  * @author ja.ortega
  */
+@Path("sucursales")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
 public class SucursalReservasResource {
     
     private static final Logger LOGGER = Logger.getLogger(SucursalReservasResource.class.getName());
